6cd3663a94e4b1023fa3cebf5559d130ffda9193
ilydotpot <applestasteveryyummy@gmail.com>
2019-02-25 21:03:31 -0800

    In main method, load education.csv file and display contents to make sure it works

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@
  * @author dottie
  */
 public class Main {
+    public static void main(String[] args) {
+        String str = Utils.readFileAsString("data/Education.csv");
+        System.out.println(str);
+    }
 }
