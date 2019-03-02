5e86cbc6ea857505c86a557c6db55decb393945f
ilydotpot <applestasteveryyummy@gmail.com>
2019-03-01 22:54:59 -0800

    Tested parse2016PresidentialResults method in Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+
 /***
  * Main class for data parsers
  * @author dottie
  */
 public class Main {
     public static void main(String[] args) {
-        String str = Utils.readFileAsString("data/Education.csv");
+        String str = Utils.readFileAsString("data/2016_Presidential_Results.csv");
         System.out.println(str);
+
+        ArrayList<ElectionResult> results = Utils.parse2016PresidentialResults(str);
+        for (ElectionResult result : results) {
+            System.out.println(result.toString());
+        }
     }
 }
