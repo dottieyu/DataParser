9266c3c15037a6ba857d16df2b8e7059848fe92a
ilydotpot <applestasteveryyummy@gmail.com>
2019-03-09 00:35:16 -0800

    too many indexoutofbounds errors. starting over

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        String str = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        String str = Utils.readFileAsString("data/Education.csv");
         // System.out.println(str);
 
-        ArrayList<ElectionResult> results = Utils.parse2016PresidentialResults(str);
-        for (ElectionResult result : results) {
+//        ArrayList<ElectionResult> results = Utils.parse2016PresidentialResults(str);
+//        for (ElectionResult result : results) {
+//            System.out.println(result.toString());
+//        }
+
+        ArrayList<EducationResult> results = Utils.parse2016EducationResults(str);
+        for (EducationResult result : results) {
             System.out.println(result.toString());
         }
     }
